b527ae88dff2ca0aa5f8f5efad39818b0dd5fb5b
jflorchi <jordan.florchinger@alumni.uleth.ca>
2022-03-18 12:24:47 -0700

    enable all cores

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -3,6 +3,9 @@
 export FINGERPRINT="TOYOTA COROLLA 2010"
 export SKIP_FW_QUERY="True"
 
+echo $$ > /dev/cpuset/app/tasks
+echo $PPID > /dev/cpuset/app/tasks
+
 export PASSIVE="0"
 exec ./launch_chffrplus.sh
 
