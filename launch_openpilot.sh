15aff52f9297d0b40d9fe16c8f4d2ea70adb191b
jflorchi <jordan.florchinger@alumni.uleth.ca>
2022-04-05 13:35:40 -0600

    corolla 2010

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/bash
 
+export FINGERPRINT="TOYOTA COROLLA 2010"
+export SKIP_FW_QUERY="True"
+
+echo $$ > /dev/cpuset/app/tasks
+echo $PPID > /dev/cpuset/app/tasks
+
 export PASSIVE="0"
 exec ./launch_chffrplus.sh
 
